f1f258e51de98d6e4b373c31ceb9846c591a0092
Alok Vaishya <alokp0913@gmail.com>
2025-07-30 23:30:55 +0530

    Rgs Added

--- main.tf
+++ main.tf
@@ -12,3 +12,18 @@ provider "azurerm" {
   subscription_id = "325c87ce-35de-4168-8c86-aad2f1fbf3f6"
 }
 
+resource "azurerm_resource_group" "RG1" {
+  name     = "aloku88374"
+  location = "east us"
+}
+
+
+resource "azurerm_resource_group" "RG1" {
+  name     = "aloku8837ewfds4"
+  location = "east us"
+}
+
+resource "azurerm_resource_group" "RG3" {
+  name     = "aloku8837awe4"
+  location = "east us"
+}
